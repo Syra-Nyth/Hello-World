0cf764cda8966b5d3399e21f142b25a4d9ac0c85
Syra-Nyth <48935704+Syra-Nyth@users.noreply.github.com>
2020-03-31 18:55:13 +1300

    Update index.svelte

--- src/index.svelte
+++ src/index.svelte
@@ -1,7 +1,14 @@
 <script>
     let name = 'Katherine'
+    let protag = "Emily"
+    let noun = "window"
+    let adjec = "ferocious"
+    let animal = "bear"
+
 </script>
 
 <h1 class="title">Svelte</h1>
 
 <p>Welcome to coding with Svelte, {name}!</p>
+
+<p>"{protag}!" they scolded, as she swung through the {noun} and rolled to a stop beside her {adjec} {animal}.</p>
